92c7ce92de03f48669540541a57fee20aa627141
sumitjhalava <sumitjhalava567@gmail.com>
2026-08-05 16:24:13 +0530

    Add class and object examples

---
+++ OOPS/Class and Object/Pen.java
@@ -0,0 +1,14 @@
+public class Pen
+{
+    String brand = "camlin";
+    int price = 40;
+    String color;
+
+    void display()
+    {
+        System.out.println("Brand : " + brand);
+        System.out.println("Price : " + price);
+        System.out.println("Color: " + color);
+        
+    }
+}
---
+++ OOPS/Class and Object/Runner3.java
@@ -0,0 +1,19 @@
+public class Runner3
+{
+    public static void main(String[] args)
+    {
+        Pen p1 = new Pen();
+        Pen p2 = new Pen();
+        Pen p3 = new Pen();
+
+        p1.color = "Red";
+        p2.color = "Blue";
+        p3.color = "Black";
+        p1.display();
+        p2.display();
+        p3.display();
+        
+
+    }
+  
+}
