9a24cbff21661407f3276ed230763148de4f5bf7
sumitjhalava <sumitjhalava567@gmail.com>
2026-08-11 21:52:03 +0530

    11-08-26

---
+++ Inheritance/Run.java
@@ -0,0 +1,18 @@
+class Run 
+{
+    public static void main(String[] agrs)
+    {
+        Car c1 = new Car();
+        c1.model = "BMW";
+        c1.color="red";
+        c1.price=334543.4;
+
+        Bike b1 = new Bike();
+        b1.model="duke";
+        b1.color="pink";
+        b1.price=234.4;
+
+        c1.display();
+        b1.display()
+    }
+}
---
+++ Inheritance/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle
+{
+    String model;
+    String color;
+    Double price;
+
+    void display()
+    {
+        System.out.println(model + " "  + color +  " " +  price);
+    }
+}
